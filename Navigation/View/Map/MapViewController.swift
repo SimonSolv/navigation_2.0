bc7a6aff9223b4e002e65fac4e9b61739ad89def
SimonSolv <4to5work@gmail.com>
2023-01-18 18:11:47 +0300

    Leaving branch

--- Navigation/View/Map/MapViewController.swift
+++ Navigation/View/Map/MapViewController.swift
@@ -467,21 +467,4 @@ extension MapViewController {
     
 }
 
-final class AnnotationForMap: NSObject, MKAnnotation {
-    
-    var title: String?
-    var coordinate: CLLocationCoordinate2D
-    var info: String
-    
-    init(
-        title: String,
-        coordinate: CLLocationCoordinate2D,
-        info: String
-    ) {
-        self.title = title
-        self.coordinate = coordinate
-        self.info = info
-        
-        super.init()
-    }
-}
+
